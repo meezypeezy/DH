8a8f34e99b2088a26dfab68c332aec219943e8b4
meezypeezy <72401075+meezypeezy@users.noreply.github.com>
2023-03-08 10:42:29 -0500

    Create formats-data.ts

---
+++ data/mods/madchemists/formats-data.ts
@@ -0,0 +1,14 @@
+export const FormatsData: {[k: string]: SpeciesFormatsData} = {
+  glimmora: {
+		tier: "MC",
+	},
+	cramorant: {
+		tier: "MC",
+	},
+	skeldrake: {
+		tier: "MC",
+	},
+	revavroom: {
+		tier: "MC",
+	},
+};
